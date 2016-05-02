96391bbe23e6e4a7c26b5f38fc820ed27e851b5b
DiegoAMedina <DiegoAMedina@users.noreply.github.com>
2016-05-02 15:20:24 -0700

    UPDATED-reads pinpoints all locations in profile
    Just need to combine it with the gui.
    make it cleaner, making functions out of the if and else so it looks better and don't repeat so much code.

--- preFinal.py
+++ preFinal.py
@@ -1,61 +1,227 @@
-# prints all the information of the user including the latitude and location
-# need to change the information into a csv file to retrieve the latitude and location
+# (33 Latest pictures)
+
+# prints all the information of the user including the latitude and location.
+# need to change the information into a csv file to retrieve the latitude and location.
 
 from lxml import etree
 import urllib
 
-# webPage will receive information from the url
-webPage = urllib.urlopen("https://api.instagram.com/v1/users/186357295/media/recent/?access_token=186357295.1677ed0.6ce9f99b45fc4ecfb460fd12acc0653f")
 
-#pageinfo will hold all the data displayed on the screen with the latitude and longitude
+#--------------------------------------------------retrieving user information---------------------------------------------------
+
+# user information to look up.
+userId = "186357295"
+
+# instagram api url to retrieve user picture information.
+url = "https://api.instagram.com/v1/users//media/recent/?access_token=186357295.1677ed0.6ce9f99b45fc4ecfb460fd12acc0653f&count=33"
+
+# insterts user id in url.
+userIdIndex = url.find("users/") + 6
+instagramUserURL = url[:userIdIndex] + userId + url[userIdIndex:]
+
+
+
+# webPage variable will hold the information from the url.
+webPage = urllib.urlopen(instagramUserURL)
+
+# pageinfo will hold all the data displayed on the screen with the latitude and longitude.
 pageInfo = webPage.read()
 
+
+# (next comment is only used for debugging - "print pageInfo").
 #print pageInfo
 
 
+# string for entire list of coordinates.
+coordinatesString = " ["
+
+# counter variable used to find every latitude and longitude within the url data without repeating any.
+counter = 0
+
+#--------------------------------------------------retrieves max_id--------------------------------------------------------------
+
+maxIdIndex = pageInfo.find("max_id=") + 7
+maxID = ""
+
+while(pageInfo[maxIdIndex] != '"'):
+	maxID += pageInfo[maxIdIndex]
+	maxIdIndex = maxIdIndex + 1
+
+#print maxID
 
 
-#--------------------------------------------------------Finds latitude
+#--------------------------------------------------------Finds coordinates (LOOP)-------------------------------------------------
+
+quitLoop = 0
+#while (pageInfo.find("max_id=") > 0):
+while (quitLoop == 0):
+
+	if (pageInfo.find("max_id=") > 0):
+
+		# loops through all the data until all the coordinates are found
+		# " > 0 " so it does not become an infinite loop 
+		while (pageInfo.find("latitude", counter) > 0 ):
 
 
-# finds the latitude index
-latitudeIndex = pageInfo.find("latitude")
-latitudeIndex = latitudeIndex + 10
+			#-------------------------------------------------------finds latitude
 
-# empty string to add the latitude into type string
-latitudeString = "["
+			# finds the latitude index
+			latitudeIndex = pageInfo.find("latitude", counter)
+			latitudeIndex = latitudeIndex + 10
 
-# loop will retrieve and add the latitude location as a string to the variable latitudeString
-while (pageInfo[latitudeIndex] != ','):
+			# will make the next search continue where the last one was found
+			counter = latitudeIndex
 
-		latitudeString += pageInfo[latitudeIndex]
-		latitudeIndex = latitudeIndex + 1
-		
-latitudeString += ","
 	
-#-------------------------------------------------------finds longitude
+			# empty string to add the latitude into type string
+			latitudeString = "["
 
-# finds the longitude index
-longitudeIndex = pageInfo.find("longitude")
-longitudeIndex = longitudeIndex + 11
+			# loop will retrieve and add the latitude location as a string to the variable latitudeString
+			while (pageInfo[latitudeIndex] != ','):
+		
+					latitudeString += pageInfo[latitudeIndex]
+					latitudeIndex = latitudeIndex + 1
+		
+			latitudeString += ","
+	
+			coordinatesString += latitudeString
+	
+	
+			#-------------------------------------------------------finds longitude
 
-# empty longitude to add the longitude of type string
-longitudeString = ""
+			# finds the longitude index
+			longitudeIndex = pageInfo.find("longitude", counter)
+			longitudeIndex = longitudeIndex + 11
+	
+			# empty longitude to add the longitude of type string
+			longitudeString = ""
 
-# loop will retrieve and add the longitude location as a string to the variable longitudeString
-while (pageInfo[longitudeIndex] != ','):
+			# loop will retrieve and add the longitude location as a string to the variable longitudeString
+			while (pageInfo[longitudeIndex] != ','):
 
-		longitudeString += pageInfo[longitudeIndex]
-		longitudeIndex = longitudeIndex + 1
+					longitudeString += pageInfo[longitudeIndex]
+					longitudeIndex = longitudeIndex + 1
+	
+			longitudeString += "],"
 
-longitudeString += "],"
-
-
-#------------------------------------------------------creating html file 
-
+			coordinatesString += longitudeString
 
 
-'''A simple program to create an html file froma given string,
+		#--------------------------------------------------retrieves max_id------------------------------------------------------
+
+		maxIdIndex = pageInfo.find("max_id=") + 7
+		maxID = ""
+
+		while(pageInfo[maxIdIndex] != '"'):
+			maxID += pageInfo[maxIdIndex]
+			maxIdIndex = maxIdIndex + 1
+
+		#--------------------------------------------------changes url with new max_id-------------------------------------------
+
+		# inserts user id in url.
+		maxIDindexURL = url.find("&count") + 9
+		instagramUserURLMaxID = instagramUserURL[:maxIDindexURL] + "&max_id=" + maxID
+
+
+		# webPage variable will hold the information from the url.
+		webPage = urllib.urlopen(instagramUserURLMaxID)
+
+		# pageinfo will hold all the data displayed on the screen with the latitude and longitude.
+		pageInfo = webPage.read()
+
+		counter = 0
+
+	else:
+
+
+		# loops through all the data until all the coordinates are found
+		# " > 0 " so it does not become an infinite loop 
+		while (pageInfo.find("latitude", counter) > 0 ):
+
+
+			#-------------------------------------------------------finds latitude
+
+			# finds the latitude index
+			latitudeIndex = pageInfo.find("latitude", counter)
+			latitudeIndex = latitudeIndex + 10
+
+			# will make the next search continue where the last one was found
+			counter = latitudeIndex
+
+	
+			# empty string to add the latitude into type string
+			latitudeString = "["
+
+			# loop will retrieve and add the latitude location as a string to the variable latitudeString
+			while (pageInfo[latitudeIndex] != ','):
+		
+					latitudeString += pageInfo[latitudeIndex]
+					latitudeIndex = latitudeIndex + 1
+		
+			latitudeString += ","
+	
+			coordinatesString += latitudeString
+	
+	
+			#-------------------------------------------------------finds longitude
+
+			# finds the longitude index
+			longitudeIndex = pageInfo.find("longitude", counter)
+			longitudeIndex = longitudeIndex + 11
+	
+			# empty longitude to add the longitude of type string
+			longitudeString = ""
+
+			# loop will retrieve and add the longitude location as a string to the variable longitudeString
+			while (pageInfo[longitudeIndex] != ','):
+
+					longitudeString += pageInfo[longitudeIndex]
+					longitudeIndex = longitudeIndex + 1
+	
+			longitudeString += "],"
+
+			coordinatesString += longitudeString
+
+
+		#--------------------------------------------------retrieves max_id------------------------------------------------------
+
+		maxIdIndex = pageInfo.find("max_id=") + 7
+		maxID = ""
+
+		while(pageInfo[maxIdIndex] != '"'):
+			maxID += pageInfo[maxIdIndex]
+			maxIdIndex = maxIdIndex + 1
+
+		#--------------------------------------------------changes url with new max_id-------------------------------------------
+
+		# inserts user id in url.
+		maxIDindexURL = url.find("&count") + 9
+		instagramUserURLMaxID = instagramUserURL[:maxIDindexURL] + "&max_id=" + maxID
+
+
+		# webPage variable will hold the information from the url.
+		webPage = urllib.urlopen(instagramUserURLMaxID)
+
+		# pageinfo will hold all the data displayed on the screen with the latitude and longitude.
+		pageInfo = webPage.read()
+
+		counter = 0
+		
+		quitLoop = 1
+
+
+
+coordinatesString += "];"
+
+# (next two comments are for debugging)
+#print pageInfo
+#print coordinatesString
+
+#------------------------------------------------------creating html file-------------------------------------------------------- 
+
+
+
+'''(COMMENT)creates an html file from a given string,
 and call the default web browser to display the file.'''
 
 contents = '''
@@ -63,7 +229,7 @@ contents = '''
 <html> 
 <head> 
   <meta http-equiv="content-type" content="text/html; charset=UTF-8" /> 
-  <title>Google Maps Multiple Markers</title> 
+  <title>InstaPlot</title> 
   <script src="http://maps.google.com/maps/api/js?sensor=false" 
           type="text/javascript"></script>
   <style>
@@ -113,20 +279,24 @@ contents = '''
 </html>
 '''
 
-#-----------------------------------------------------html string manipulation
+#-----------------------------------------------------html string manipulation---------------------------------------------------
 
-coordinatesString = " [" + latitudeString + longitudeString + "];"
 
+# adds the coordinates found previously in the instagra api url
+# to the html string
 index = contents.find("var locations =") + 15
 newContents = contents[:index] + coordinatesString + contents[index:]
 
 #print newContents
 
 
-#-----------------------------------------------------open webPage
+#-----------------------------------------------------open webPage----------------------------------------------------------------
+
+import webbrowser, os.path
 
 def main():
     browseLocal(newContents)
+
 
 def strToFile(text, filename):
     #Write a file with the given name and the given text.
@@ -134,14 +304,16 @@ def strToFile(text, filename):
     output.write(text)
     output.close()
 
-def browseLocal(webpageText, filename='tempBrowseLocal.html'):
+
+def browseLocal(webpageText, filename='InstaPlot.html'):
     #Start your webbrowser on a local file containing the text with given filename.
-    import webbrowser, os.path
     strToFile(webpageText, filename)
     webbrowser.open("file:///" + os.path.abspath(filename)) #elaborated for Mac
 
 main()
 
-#print contents
 
+
+# (next comment for debugging)
+#print pageInfo
 
